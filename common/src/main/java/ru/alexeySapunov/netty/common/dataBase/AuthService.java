eb50ca1d9d3430d33c67cf06acb36bf6d6071032
Alexey <blacka8607@rambler.ru>
2021-12-03 19:30:26 +0300

    Changes on DBAuthservice and ScannerAuth

--- common/src/main/java/ru/alexeySapunov/netty/common/dataBase/AuthService.java
+++
@@ -1,8 +0,0 @@
-package ru.alexeySapunov.netty.common.dataBase;
-
-import java.sql.SQLException;
-
-public interface AuthService {
-    void authClients(final String login, final String password) throws SQLException;
-    String regNewClients(final String login, final String password) throws SQLException;
-}
